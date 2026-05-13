f9eaad920ecdc73e6ad8af4a03777865ea46ad07
Bakhtiyar Khan <your-email@example.com>
2026-05-12 21:45:58 -0500

    Add backend configuration for development environment

---
+++ environments/dev/backend.tf
@@ -0,0 +1,8 @@
+﻿terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-modular-BK"
+    storage_account_name = "stterraformmodulardevbk"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
+}
